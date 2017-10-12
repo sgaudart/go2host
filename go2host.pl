7e32203661a16d056154570c9d7aa5cef7a4322b
Sebastien Gaudart <sgaudart.dev@gmail.com>
2017-10-12 14:43:52 +0200

    management of fields in conf file (1st line)

--- go2host.pl
+++ go2host.pl
@@ -10,30 +10,59 @@ my $conf="hosts.conf";
 my $filter = shift(@ARGV);
 my $sshpass="/home/sgaudart/sshpass"; # path to the command sshpass
 my $line;
+my @row;
+my @data;
 my ($id,$hostname,$ip,$login,$passwd);
-my %iphash;
+my ($id_pos,$hostname_pos,$descr_pos,$ip_pos,$login_pos,$password_pos); # position of each row in conf file
+my %sshdata;
+my $i=0; # count line in conf file
+my $index=0; # index pour @row
 
 open (FD, "$conf") or die "Can't open conf  : $conf\n" ; # reading
 while (<FD>)
 {
-	$line=$_;
-	chomp($line); # delete the carriage return
-	($id, $hostname, $ip, $login, $passwd) = split(';', $line);
-	$iphash{$id}=$ip;
-	#print "[DEBUG] iphash{$id}=$ip\n";
-	if (defined $filter)
-	{
-	   # must filter
-		 if ($line =~ /$filter/)
-		 {
-			 # filter match !
-			 print "$id\t$hostname\n";
-		 }
-	}
-	else
-	{
-     print "$id\t$hostname\n";
-	}
+   $i++;
+   $line=$_;
+   chomp($line); # delete the carriage return
+   
+   if ($i eq 1)
+   {
+      @row = split(';',$line);
+      # first line => we feed vars *_pos
+      foreach my $field (@row)
+      {
+         #print "row[$index]=$field\n";
+         if ($field eq "id") { $id_pos=$index; }
+         if ($field eq "hostname") { $hostname_pos=$index; }
+         if ($field eq "descr") { $descr_pos=$index; }
+         if ($field eq "ip") { $ip_pos=$index; }
+         if ($field eq "login") { $login_pos=$index; }
+         if ($field eq "password") { $password_pos=$index; }
+
+         $index++;
+      }
+   }
+
+   #($id, $hostname, $ip, $login, $passwd) = split(';', $line);
+   @data = split(';',$line);
+
+   $sshdata{$data[$id_pos]}{ip}=$data[$ip_pos];
+   $sshdata{$data[$id_pos]}{login}=$data[$login_pos];
+   $sshdata{$data[$id_pos]}{password}=$data[$password_pos];
+   #print "[DEBUG] iphash{$id}=$ip\n";
+   if (defined $filter)
+   {
+      # must filter 
+      if ($line =~ /$filter/)
+      {
+         # filter match !
+         print "$data[$id_pos]\t$data[$hostname_pos]\n";
+      }
+   }
+   else
+   {
+      print "$data[$id_pos]\t$data[$hostname_pos]\n";
+   }
 }
 close FD;
 
@@ -43,5 +72,5 @@ my $choice = <STDIN>;
 chomp $choice;
 if ($choice eq "") { exit; }
 
-#print "[DEBUG]: ip=$iphash{$choice}\n";
-exec("$sshpass -p $passwd ssh $login\@$iphash{$choice}");
+#print "[DEBUG]: choice=$choice sshdata{$choice}{ip}=$sshdata{$choice}{ip}\n";
+exec("$sshpass -p $sshdata{$choice}{password} ssh $sshdata{$choice}{login}\@$sshdata{$choice}{ip}");
