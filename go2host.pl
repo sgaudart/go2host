99324a2c04a8ca40521e43ef8d384c2c35fe0090
Sebastien Gaudart <sgaudart.dev@gmail.com>
2017-11-10 16:04:28 +0100

    improvement autoinc $id

--- go2host.pl
+++ go2host.pl
@@ -6,7 +6,7 @@
 #use strict;
 #use warnings;
 
-my $conf="hosts.conf";
+my $conf="hosts2.conf";
 my $filter = shift(@ARGV);
 my $sshpass="/usr/bin/sshpass"; # path to the command sshpass
 my ($line,$display,$rowname);
@@ -15,6 +15,7 @@ my @dataline; # store only the current line (format array)
 my %pos; # $pos{field} => return the index of the row field in the conf file
 my $i=0; # count line in conf file
 my $index=0; # index pour @row
+my $id=1;
 
 START:
 open (FD, "$conf") or die "Can't open conf : $conf\n" ; # reading
@@ -34,19 +35,23 @@ while (<FD>)
          $index++;
       }
    }
-  
+ 
+   if (defined $pos{id}) { $id=$dataline[$pos{id}]; }
+
+   # PREPATE %conf
    foreach $rowname (keys %pos)
    {
-      $conf{$dataline[$pos{id}]}{$rowname}=$dataline[$pos{$rowname}];
+      $conf{$id}{$rowname}=$dataline[$pos{$rowname}];
    }
    
+   # PREPARE $display
    if (defined $pos{descr}) # we have description in the conf file
    {
-      $display="printf(\"%-8s %-15s %-10s \n\", $dataline[$pos{id}], \"$dataline[$pos{hostname}]\", \"$dataline[$pos{descr}]\");";
+      $display="printf(\"%-8s %-15s %-10s \n\", $id, \"$dataline[$pos{hostname}]\", \"$dataline[$pos{descr}]\");";
    }
    else
    {
-      $display="printf(\"%-8s %-15s \n\",$dataline[$pos{id}],\"$dataline[$pos{hostname}]\");";
+      $display="printf(\"%-8s %-15s \n\",$id,\"$dataline[$pos{hostname}]\");";
    }
  
    
@@ -63,28 +68,29 @@ while (<FD>)
    {
       eval $display;
    }
+   $id++;
 
 }
 close FD;
 
 # Ask the ID to the user
 print "Type id (or filter) : ";
-my $id = <STDIN>;
-chomp $id;
-if ($id eq "") { exit; }
-if (! defined $conf{$id}{ip}) { $filter=$id; goto START; }
+my $choice = <STDIN>;
+chomp $choice;
+if ($choice eq "") { exit; }
+if (! defined $conf{$choice}{ip}) { $filter=$choice; goto START; }
 
 
 # SSH CONNECTION
 if ((defined $pos{login}) && (defined $pos{password}))
 {
    # connection with login/password
-   #print "[DEBUG]: id=$id => $sshpass -p $conf{$id}{password} ssh $conf{$id}{login}\@$conf{$id}{ip}\n";
-   exec("$sshpass -p $conf{$id}{password} ssh $conf{$id}{login}\@$conf{$id}{ip}");
+   print "[DEBUG]: id=$choice => $sshpass -p $conf{$choice}{password} ssh $conf{$choice}{login}\@$conf{$choice}{ip}\n";
+   exec("$sshpass -p $conf{$choice}{password} ssh $conf{$choice}{login}\@$conf{$choice}{ip}");
 }
 else
 {
    # connection simple
-   #print "[DEBUG]: id=$id => ssh $conf{$id}{ip}\n";
-   exec("ssh $conf{$id}{ip}");
+   print "[DEBUG]: id=$choice => ssh $conf{$choice}{ip}\n";
+   exec("ssh $conf{$choice}{ip}");
 }
