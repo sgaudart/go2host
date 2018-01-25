dc2c4df40782ff3f015660c8d9e6de31fcef51f5
Sebastien Gaudart <sgaudart.dev@gmail.com>
2018-01-25 14:25:24 +0100

    if display = single result, automatic ssh connect
    if the filter gives you a single result, automatic ssh connection

--- go2host.pl
+++ go2host.pl
@@ -9,7 +9,7 @@
 my $conf="hosts.conf";
 my $filter = shift(@ARGV);
 my $sshpass="/usr/bin/sshpass"; # path to the command sshpass
-my ($line,$i,$index,$display,$rowname);
+my ($line,$i,$displaycounter,$index,$display,$rowname,$choice);
 my %conf; # store each line of the conf file like this $conf{id}{rowname}
 my @dataline; # store only the current line (format array)
 my %pos; # $pos{field} => return the index of the row field in the conf file
@@ -17,6 +17,7 @@ my $id="id";
 
 START:
 $i=0; # init line index
+$displaycounter=0; # init $displaycounter
 open (FD, "$conf") or die "Can't open conf : $conf\n" ; # reading
 while (<FD>)
 {
@@ -24,8 +25,8 @@ while (<FD>)
    $line=$_;
    chomp($line); # delete the carriage return
    @dataline = split(';',$line); # split current line
-   
-   if ($i eq 1)
+
+   if ($i eq 1) # only for the first line
    {
       $index=0; # init
       foreach $rowname (@dataline)
@@ -35,7 +36,7 @@ while (<FD>)
          $index++;
       }
    }
- 
+
    if (defined $pos{id}) { $id=$dataline[$pos{id}]; }
 
    # PREPARE %conf
@@ -53,14 +54,16 @@ while (<FD>)
    {
       $display="printf(\"%-8s %-15s \n\",$id,\"$dataline[$pos{hostname}]\");";
    }
- 
-   
+
+   # DISPLAY
    if (defined $filter)
    {
       # filter case
       if (($line =~ /$filter/) || ($i eq 1))
       {
          # filter match !
+         $displaycounter++;
+         $choice=$id;
          eval $display;
       }
    }
@@ -75,15 +78,18 @@ while (<FD>)
 }
 close FD;
 
-# Ask the ID to the user
-print "Type id (or filter) : ";
-my $choice = <STDIN>;
-chomp $choice;
-if ($choice eq "") { exit; } # no choice => exit
-if (! defined $conf{$choice}{hostname}) { $filter=$choice; goto START; } # not id => filter
+
+if ($displaycounter ne 2)
+{
+   # Ask the ID to the user
+   print "Type id (or filter) : ";
+   $choice = <STDIN>;
+   chomp $choice;
+   if ($choice eq "") { exit; } # no choice => exit
+   if (! defined $conf{$choice}{hostname}) { $filter=$choice; goto START; } # not id => filter
+}
+
 if (! defined $conf{$choice}{ip}) { $conf{$choice}{ip} = $conf{$choice}{hostname}; } # no known ip => use hostname for ssh
-
-
 # SSH CONNECTION
 if ((defined $pos{login}) && (defined $pos{password}))
 {
@@ -93,7 +99,7 @@ if ((defined $pos{login}) && (defined $pos{password}))
 }
 else
 {
-   # connection simple
+   # simple connection
    #print "[DEBUG]: id=$choice => ssh $conf{$choice}{ip}\n";
    exec("ssh $conf{$choice}{ip}");
 }
